6e1a337a278657fb0f9bfe33b3a5ee2e90253daf
Denis <dsboikov@yandex.ru>
2025-09-13 21:23:03 +0300

    мелкие доработки

--- orders/forms.py
+++ orders/forms.py
@@ -4,5 +4,5 @@ from django import forms
 
 class CheckoutForm(forms.Form):
     shipping_address = forms.CharField(widget=forms.Textarea(attrs={"class": "form-control", "rows": 3}))
-    payment_method = forms.ChoiceField(choices=[("mock", "Оплата при получении (эмуляция)")],
+    payment_method = forms.ChoiceField(choices=[("mock", "Оплата при получении")],
                                        widget=forms.Select(attrs={"class": "form-select"}))
